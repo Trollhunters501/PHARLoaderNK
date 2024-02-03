4b9c549225353f182c9aafa7efedb7bb2ff1cb25
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-02-02 22:50:10 -0600

    Create PHARLoaderNK.js
    New!

---
+++ src/Creadores/Program/PHARLoaderNK.js
@@ -0,0 +1,31 @@
+if(script.getScriptByName("PHPEngineNK")){
+const PHARLoaderNK = Class(Object, {
+  constructor: function(fileP){
+    let lib = new NnClassLoader({ urls: ["https://github.com/npetrovski/jphar/releases/download/2.0.1/jphar-2.0.1.jar"] });
+    let PHARLib = lib.type("name.npetrovski.jphar.Phar");
+    let PharF = new PHARLib(fileP);
+    let subClass = {
+      getPhar: function(){
+        return PharF;
+      },
+      evalPHP: function(dir, PHPEngineNKContext){
+        let fileSearch = PharF.findEntry(dir);
+        if(fileSearch == null){
+          throw "The file was not found in the PHAR!";
+        }
+        let inputFile = fileSearch.getInputStream();
+        let FilesImport = new JavaImporter(java.io, java.nio.charset);
+        let reader;
+        with(FilesImport){
+          reader = new InputStreamReader(new BufferedInputStream(inputFile), StandardCharsets.UTF_8);
+        }
+        PHPEngineNKContext.eval(reader);
+      }
+    };
+    return subClass;
+  }
+});
+}else{
+  console.error("§c Could not load PHARLoaderNK Library");
+  throw "PHPEngineNK Library Not Found!";
+}
