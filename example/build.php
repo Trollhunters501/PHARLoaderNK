e319014653410a030caa98d9bffaa957f297c1c2
Trollhunters501PC <cpcreador@gmail.com>
2024-12-01 22:07:12 -0600

    Test 2

--- example/build.php
+++ example/build.php
@@ -2,11 +2,15 @@
 echo "Empaquetando PHAR...\n";
 $pharFile = "Example.phar";
 $phar = new Phar($pharFile);
+$phar->setAlias("Example-1.1.1.phar");
 $phar->startBuffering();
 $phar->buildFromDirectory('Plugin');
-$phar->stopBuffering();
 if (Phar::canCompress(Phar::GZ)) {
     $phar->compressFiles(Phar::GZ);
+} elseif (Phar::canCompress(Phar::BZ2)) { 
+    $phar->compressFiles(Phar::BZ2); 
 }
+$phar->setSignatureAlgorithm(Phar::SHA512);
+$phar->stopBuffering();
 echo "Done!\n";
 ?>
